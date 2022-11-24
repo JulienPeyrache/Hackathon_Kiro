36801bfe4ae19f46dd95b55fed162b99ac993de4
Julien Peyrache <julien.peyrache@student-cs.fr>
2022-11-24 16:31:03 +0100

    commentaires var

--- optimizer.py
+++ optimizer.py
@@ -143,27 +143,27 @@ for i in tasks_per_job:
 
 ### Decision variables
 # beginning of tasks of jobs
-B = {(j,k) : m.addVar(vtype = GRB.INTEGER, name = f'b_{j}_{k}') for j in range(n_jobs) for k in range(n_tasks_per_job[j])}
+B = {(j,k) : m.addVar(vtype = GRB.INTEGER, name = f'b_{j}_{k}') for j in range(n_jobs) for k in range(tasks_per_job[j])}
 
 # ending of tasks of jobs
 # c = {(j,k) : m.addVar(vtype = GRB.INTEGER, name = f'c_{j}_{k}') for j in range(n_jobs) for k in range(n_tasks_per_job[j])}
 
 # tardiness of jobs
-T = {j : m.addVar(vtype = GRB.INTEGER, name = f't_{j}_{k}') for j in range(n_jobs) for k in range(n_tasks_per_job[j])}
-U = {(j,k) : m.addVar(vtype = GRB.BINARY, name = f'u_{j}_{k}') for j in range(n_jobs) for k in range(n_tasks_per_job[j])}
+T = {j : m.addVar(vtype = GRB.INTEGER, name = f't_{j}_{k}') for j in range(n_jobs) for k in range(tasks_per_job[j])}
+U = {(j,k) : m.addVar(vtype = GRB.BINARY, name = f'u_{j}_{k}') for j in range(n_jobs) for k in range(tasks_per_job[j])}
 
 # Task-machine assignment
-M = {(j,k) : m.addVar(vtype = GRB.INTEGER, name = f'm_{j}_{k}') for j in range(n_jobs) for k in range(n_tasks_per_job[j])}
+M = {(j,k) : m.addVar(vtype = GRB.INTEGER, name = f'm_{j}_{k}') for j in range(n_jobs) for k in range(tasks_per_job[j])}
 
 # Task-operator assignment
-O = {(j,k) : m.addVar(vtype = GRB.INTEGER, name = f'o_{j}_{k}') for j in range(n_jobs) for k in range(n_tasks_per_job[j])}
+O = {(j,k) : m.addVar(vtype = GRB.INTEGER, name = f'o_{j}_{k}') for j in range(n_jobs) for k in range(tasks_per_job[j])}
 
 # One Task indicator
-Y = {(j,k,jp,kp) : m.addVar(vtype = GRB.BINARY, name = f'o_{j}_{k}_{jp}_{kp}') for j in range(n_jobs) for k in range(n_tasks_per_job[j]) for jp in range(n_jobs) for kp in range(n_tasks_per_job[jp])}
+Y = {(j,k,jp,kp) : m.addVar(vtype = GRB.BINARY, name = f'o_{j}_{k}_{jp}_{kp}') for j in range(n_jobs) for k in range(tasks_per_job[j]) for jp in range(n_jobs) for kp in range(tasks_per_job[jp])}
 
 
 for j in range(1,n_jobs):
-  for k in  range(2,n_tasks_per_job[j]+1):
+  for k in  range(2,tasks_per_job[j]+1):
     m.addConstr(B[(j,k)] >= B[(j,k-1)]+p[(k,j)])
 
 for j in range(1,n_jobs+1):
