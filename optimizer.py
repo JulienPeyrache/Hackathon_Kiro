9b937fcc8b8471f251211bfcffafca36d1f99e33
Julien Peyrache <julien.peyrache@student-cs.fr>
2022-11-24 15:47:41 +0100

    optimizer conflict

--- optimizer.py
+++ optimizer.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from gurobipy import *
 
 m = Model("trs0")
@@ -13,31 +12,46 @@ n_tasks_per_job = [i for i in range(10)]
 
 ### Decision variables
 # beginning of tasks of jobs
-b = {(j,k) : m.addVar(vtype = GRB.INTEGER, name = f'b_{j}_{k}') for j in range(n_jobs) for k in range(n_tasks_per_job[j])}
+b = {
+    (j, k): m.addVar(vtype=GRB.INTEGER, name=f"b_{j}_{k}")
+    for j in range(n_jobs)
+    for k in range(n_tasks_per_job[j])
+}
 
 # ending of tasks of jobs
 # c = {(j,k) : m.addVar(vtype = GRB.INTEGER, name = f'c_{j}_{k}') for j in range(n_jobs) for k in range(n_tasks_per_job[j])}
 
 # tardiness of jobs
-t = {j : m.addVar(vtype = GRB.INTEGER, name = f't_{j}_{k}') for j in range(n_jobs) for k in range(n_tasks_per_job[j])}
-u = {(j,k) : m.addVar(vtype = GRB.BINARY, name = f'u_{j}_{k}') for j in range(n_jobs) for k in range(n_tasks_per_job[j])}
+t = {
+    j: m.addVar(vtype=GRB.INTEGER, name=f"t_{j}_{k}")
+    for j in range(n_jobs)
+    for k in range(n_tasks_per_job[j])
+}
+u = {
+    (j, k): m.addVar(vtype=GRB.BINARY, name=f"u_{j}_{k}")
+    for j in range(n_jobs)
+    for k in range(n_tasks_per_job[j])
+}
 
 # Task-machine assignment
-m = {(j,k) : m.addVar(vtype = GRB.INTEGER, name = f'm_{j}_{k}') for j in range(n_jobs) for k in range(n_tasks_per_job[j])}
+m = {
+    (j, k): m.addVar(vtype=GRB.INTEGER, name=f"m_{j}_{k}")
+    for j in range(n_jobs)
+    for k in range(n_tasks_per_job[j])
+}
 
 # Task-operator assignment
-o = {(j,k) : m.addVar(vtype = GRB.INTEGER, name = f'o_{j}_{k}') for j in range(n_jobs) for k in range(n_tasks_per_job[j])}
+o = {
+    (j, k): m.addVar(vtype=GRB.INTEGER, name=f"o_{j}_{k}")
+    for j in range(n_jobs)
+    for k in range(n_tasks_per_job[j])
+}
 
 # One Task indicator
-y = {(j,k,jp,kp) : m.addVar(vtype = GRB.BINARY, name = f'o_{j}_{k}_{jp}_{kp}') for j in range(n_jobs) for k in range(n_tasks_per_job[j]) for jp in range(n_jobs) for kp in range(n_tasks_per_job[jp])}
-=======
-import gurobipy as gp 
-from gurobipy import GRB
-
-try:
-    #Create a new model
-    m = gp.Model("mip1")
-
-    #Create variables
-    x = m.addVar(vtype=GRB.INTEGER, name="x")
->>>>>>> 1bb963c (optimizer julien)
+y = {
+    (j, k, jp, kp): m.addVar(vtype=GRB.BINARY, name=f"o_{j}_{k}_{jp}_{kp}")
+    for j in range(n_jobs)
+    for k in range(n_tasks_per_job[j])
+    for jp in range(n_jobs)
+    for kp in range(n_tasks_per_job[jp])
+}
