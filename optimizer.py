1821736c3fdbfb8771259410f824fc09ac02a361
Julien Peyrache <julien.peyrache@student-cs.fr>
2022-11-24 16:32:41 +0100

    debug

--- optimizer.py
+++ optimizer.py
@@ -2,6 +2,7 @@ from functools import reduce
 import re
 import pprint
 
+
 def array_parser(data):
     if data[0] != "[":
         return None
@@ -20,23 +21,28 @@ def array_parser(data):
             return None
         data = res[1].strip()
 
+
 def boolean_parser(data):
     if data[0:4] == "true":
         return [True, data[4:].strip()]
     elif data[0:5] == "false":
         return [False, data[5:].strip()]
 
+
 def colon_parser(data):
     if data[0] == ":":
         return [data[0], data[1:].lstrip()]
+
 
 def comma_parser(data):
     if data and data[0] == ",":
         return [data[0], data[1:].strip()]
 
+
 def null_parser(data):
     if data[0:4] == "null":
         return [None, data[4:].strip()]
+
 
 def number_parser(data):
     regex_find = re.findall("^(-?(?:[0-9]\d*)(?:\.\d+)?(?:[eE][+-]?\d+)?)", data)
@@ -47,6 +53,7 @@ def number_parser(data):
         return [int(regex_find[0]), data[pos:].strip()]
     except ValueError:
         return [float(regex_find[0]), data[pos:].strip()]
+
 
 def object_parser(data):
     if data[0] != "{":
@@ -71,14 +78,17 @@ def object_parser(data):
             data = res[1].strip()
     return [parse_dict, data[1:]]
 
+
 def string_parser(data):
     if data[0] == '"':
         data = data[1:]
         pos = data.find('"')
         return [data[:pos], data[pos + 1 :].strip()]
 
+
 def all_parsers(*args):
     return lambda data: (reduce(lambda f, g: f if f(data) else g, args)(data))
+
 
 value_parser = all_parsers(
     null_parser,
@@ -88,15 +98,10 @@ value_parser = all_parsers(
     object_parser,
     array_parser,
 )
-<<<<<<< HEAD
-def parser():
-    with open("sujet/tiny.json", "r") as f:
-=======
 
 
 def parser(filename):
     with open(filename, "r") as f:
->>>>>>> 912c9e5 (variablesé)
         data = f.read()
     res = value_parser(data.strip())
     try:
@@ -104,7 +109,8 @@ def parser(filename):
     except TypeError:
         print(None)
 
-json = parser()
+
+json = parser("sujet/tiny.json")
 
 from gurobipy import Model
 
@@ -114,58 +120,95 @@ m = Model("trs0")
 M = 1000000000
 
 
-n_machines = json["parameters"]["size"]["nb_machines"] ##Nombre de machines
-n_operators = json["parameters"]["size"]["nb_operators"] ##Nombre d'opérateurs
-n_jobs = json["parameters"]["size"]["nb_jobs"] ##Nombre de jobs
-job_names = [json["jobs"][i]["job"] for i in range(n_jobs)] ##Nom effectif des jobs
+n_machines = json["parameters"]["size"]["nb_machines"]  ##Nombre de machines
+n_operators = json["parameters"]["size"]["nb_operators"]  ##Nombre d'opérateurs
+n_jobs = json["parameters"]["size"]["nb_jobs"]  ##Nombre de jobs
+job_names = [json["jobs"][i]["job"] for i in range(n_jobs)]  ##Nom effectif des jobs
 
 
-tasks_per_job ={} ##Dictio des tâches par job (clé = nom du job)
+tasks_per_job = {}  ##Dictio des tâches par job (clé = nom du job)
 for i in range(n_jobs):
-    tasks_per_job[json["jobs"][i]["job"]] = json["jobs"][i]["sequence"] ##Nombre de tâches par job
+    tasks_per_job[json["jobs"][i]["job"]] = json["jobs"][i][
+        "sequence"
+    ]  ##Nombre de tâches par job
 
-w={} ##Dictio des poids des job (clé = nom du job)
-d={} ##Dictio des durées des jobs (clé = nom du job)
+w = {}  ##Dictio des poids des job (clé = nom du job)
+d = {}  ##Dictio des durées des jobs (clé = nom du job)
 r = {}  ##Dictio des ressources des jobs (clé = nom du job)
 for i in range(n_jobs):
-    w[json["jobs"][i]["job"]] = json["jobs"][i]["weight"] ##Poids des jobs
-    d[json["jobs"][i]["job"]] = json["jobs"][i]["due_date"] ##Date limite de fin des jobs
-    r[json["jobs"][i]["job"]] = json["jobs"][i]["release_date"] ##Date de début des jobs
+    w[json["jobs"][i]["job"]] = json["jobs"][i]["weight"]  ##Poids des jobs
+    d[json["jobs"][i]["job"]] = json["jobs"][i][
+        "due_date"
+    ]  ##Date limite de fin des jobs
+    r[json["jobs"][i]["job"]] = json["jobs"][i][
+        "release_date"
+    ]  ##Date de début des jobs
 
-p_tasks = {} ##Dictio des durées des tâches (clé = nom de la tâche)
-op_tasks = {} ##Dictio des opérateurs des tâches (clé = nom de la tâche)
-m_tasks = {} ##Dictio des machines des tâches (clé = nom de la tâche)
+p_tasks = {}  ##Dictio des durées des tâches (clé = nom de la tâche)
+op_tasks = {}  ##Dictio des opérateurs des tâches (clé = nom de la tâche)
+m_tasks = {}  ##Dictio des machines des tâches (clé = nom de la tâche)
 for i in tasks_per_job:
     for j in tasks_per_job[i]:
-        p_tasks[j] = json["tasks"][j]["processing_time"] ##Dictionnaire des processing time par tâche
-        m_tasks[j] = json["tasks"][j]["machine"] ##Dictionnaire des machine par id de tâche
-        op_tasks[j] = json["tasks"][j]["operator"] ##Dictionnaire des opérateurs par id de tâche
+        p_tasks[j] = json["tasks"][j][
+            "processing_time"
+        ]  ##Dictionnaire des processing time par tâche
+        m_tasks[j] = json["tasks"][j][
+            "machine"
+        ]  ##Dictionnaire des machine par id de tâche
+        op_tasks[j] = json["tasks"][j][
+            "operator"
+        ]  ##Dictionnaire des opérateurs par id de tâche
 
 ### Decision variables
 # beginning of tasks of jobs
-B = {(j,k) : m.addVar(vtype = GRB.INTEGER, name = f'b_{j}_{k}') for j in range(n_jobs) for k in range(tasks_per_job[j])}
+B = {
+    (j, k): m.addVar(vtype=GRB.INTEGER, name=f"b_{j}_{k}")
+    for j in range(n_jobs)
+    for k in range(tasks_per_job[j])
+}
 
 # ending of tasks of jobs
 # c = {(j,k) : m.addVar(vtype = GRB.INTEGER, name = f'c_{j}_{k}') for j in range(n_jobs) for k in range(n_tasks_per_job[j])}
 
 # tardiness of jobs
-T = {j : m.addVar(vtype = GRB.INTEGER, name = f't_{j}_{k}') for j in range(n_jobs) for k in range(tasks_per_job[j])}
-U = {(j,k) : m.addVar(vtype = GRB.BINARY, name = f'u_{j}_{k}') for j in range(n_jobs) for k in range(tasks_per_job[j])}
+T = {
+    j: m.addVar(vtype=GRB.INTEGER, name=f"t_{j}_{k}")
+    for j in range(n_jobs)
+    for k in range(tasks_per_job[j])
+}
+U = {
+    (j, k): m.addVar(vtype=GRB.BINARY, name=f"u_{j}_{k}")
+    for j in range(n_jobs)
+    for k in range(tasks_per_job[j])
+}
 
 # Task-machine assignment
-M = {(j,k) : m.addVar(vtype = GRB.INTEGER, name = f'm_{j}_{k}') for j in range(n_jobs) for k in range(tasks_per_job[j])}
+M = {
+    (j, k): m.addVar(vtype=GRB.INTEGER, name=f"m_{j}_{k}")
+    for j in range(n_jobs)
+    for k in range(tasks_per_job[j])
+}
 
 # Task-operator assignment
-O = {(j,k) : m.addVar(vtype = GRB.INTEGER, name = f'o_{j}_{k}') for j in range(n_jobs) for k in range(tasks_per_job[j])}
+O = {
+    (j, k): m.addVar(vtype=GRB.INTEGER, name=f"o_{j}_{k}")
+    for j in range(n_jobs)
+    for k in range(tasks_per_job[j])
+}
 
 # One Task indicator
-Y = {(j,k,jp,kp) : m.addVar(vtype = GRB.BINARY, name = f'o_{j}_{k}_{jp}_{kp}') for j in range(n_jobs) for k in range(tasks_per_job[j]) for jp in range(n_jobs) for kp in range(tasks_per_job[jp])}
+Y = {
+    (j, k, jp, kp): m.addVar(vtype=GRB.BINARY, name=f"o_{j}_{k}_{jp}_{kp}")
+    for j in range(n_jobs)
+    for k in range(tasks_per_job[j])
+    for jp in range(n_jobs)
+    for kp in range(tasks_per_job[jp])
+}
 
 
-for j in range(1,n_jobs):
-  for k in  range(2,tasks_per_job[j]+1):
-    m.addConstr(B[(j,k)] >= B[(j,k-1)]+p[(k,j)])
+for j in range(1, n_jobs):
+    for k in range(2, tasks_per_job[j] + 1):
+        m.addConstr(B[(j, k)] >= B[(j, k - 1)] + p_tasks[(k, j)])
 
-for j in range(1,n_jobs+1):
-  m.addConstr(B[(j,2)] >= r[j])
-
+for j in range(1, n_jobs + 1):
+    m.addConstr(B[(j, 2)] >= r[j])
